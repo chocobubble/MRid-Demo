321b6cb34804aa6c7aef2f8771e33c7c67540cb1
chocobubble <kyaya12345@gmail.com>
2023-04-03 23:46:14 +0900

    Create QuestSO Modify LevelSO

--- Assets/Scripts/Data/QuestSO.cs
+++ Assets/Scripts/Data/QuestSO.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
     // holds basic level information (label name, level number, scene name for loading, thumbnail graphic for display, etc.)
-    [CreateAssetMenu(fileName = "Assets/Resources/GameData/Levels/LevelData", menuName = "MRid/Level", order = 11)]
-    public class LevelSO : ScriptableObject
+    [CreateAssetMenu(fileName = "Assets/Resources/GameData/Quest/QuestData", menuName = "MRid/Quest", order = 11)]
+    public class QuestSO : ScriptableObject
     {
-        public string dungeonName;
-        public int dungeonNumber;
-        public int dungeonHardness;
-        public Sprite dungeonSprite;
+        public int levelNumber;
+        public string levelHardness;
+        public Sprite thumbnail;
         public string sceneName;
         public int goldEarning;
         public int xpGetting;
